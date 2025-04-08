689fa3c5acd85d9474c26e17dae0c56db34b214b
glendawei <glendaliuwei@gmail.com>
2025-04-09 05:14:11 +0800

    try out

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ import psycopg2
 import logging
 def get_db_connection():
     return psycopg2.connect(
-        dbname="IMBANK_TRY",
+        dbname="IMBANK",
         user="postgres",
         password="705018",
         host="localhost",
